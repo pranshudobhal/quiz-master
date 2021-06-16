2a161b90c9326b5e1645d5730717a24cc6ad5dea
Pranshu Dobhal <pranshu.dobhal@gmail.com>
2021-06-16 22:09:58 +0530

    updated loader

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -4,6 +4,7 @@ export function Loader() {
   return (
     <div className={styles.container}>
       <div className={styles.loader}></div>
+      <div style={{ textAlign: 'center', fontSize: '1.75rem' }}>Please wait while we load the quiz!</div>
     </div>
   );
 }
